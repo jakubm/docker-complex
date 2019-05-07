42540fc20ea2737268d62b5f32dad2386ebf8044
Jakub Mišina <jakub.misina@gmail.com>
2019-05-07 19:58:53 +0200

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import OtherPage from './OtherPage'
+import Fib from './Fib'
+function App() {
+  return (
+    <Router>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/otherpage">Other Page</Link>
+      </header>
+      <div>
+        <Route exact path="/" component={Fib}/>
+        <Route exact path="/otherpage" component={OtherPage}/>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
